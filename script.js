99db3b10b7b1dd1a17ae10c346a18c9eaaaa69ea
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-10 16:52:53 +0545

    activity-arrays in js

---
+++ script.js
@@ -0,0 +1,15 @@
+var ar=["apple","mango","orange","banana"]
+console.log(ar.length)//length
+document.getElementById("a").innerHTML = ar[2] + " is my favorite fruit."
+
+ar.push("pineapple")
+console.log(ar)
+
+ar.pop()
+console.log(ar)
+
+
+console.log(ar.join("/"))
+
+console.log(ar[ar.length-1])
+
---
+++ script1.js
@@ -0,0 +1,8 @@
+function add(a,b){
+    return a+b
+}
+function average(a,b){
+    return (a+b)/2
+}
+result=average(10,20)
+console.log(result)
